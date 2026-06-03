7dc9e6445b6b406f56e47b08d6efbb5a22c9ff9b
Dashboard <janavigodase57@gmail.com>
2026-06-03 22:15:56 +0530

    functions

---
+++ array.js
@@ -0,0 +1,22 @@
+let arrayname=["janavi","sneha","priya","samuu"];
+console.log(arrayname[0]);
+console.log(arrayname[1]);
+console.log(arrayname[2]);
+console.log(arrayname[3]);  
+
+
+let departments=["CSE","ECE","MECH","CIVIL"];
+for(let i=0;i<departments.length;i++){
+    console.log(departments[i]);
+}   
+
+
+let table=[2,,4,6,8,10,12,14,16,18,20];
+for(let i=0;i<table.length;i++){
+    console.log(table[i]);
+}   
+
+let arr=[1,2];
+arr.push(3);
+
+
---
+++ function.js
@@ -0,0 +1,52 @@
+
+// Function Declaration
+function greet1(name) {
+    console.log("Hello, " + name + "!");
+} 
+
+greet1("janavi");
+
+// Function Expression
+const greet=function(){
+    console.log("Hello");
+}   
+greet();
+
+// Arrow Function
+const add=(a,b)=>{
+    return a+b;
+};
+console.log(add(5,3));
+
+
+//anonymous function
+const greet2=function(){
+    console.log("Hello, World!");
+}
+greet2();
+
+
+
+function anonymousGreet=function(name,anotherFunction){
+    console.log("Hello, " + name + "!");
+    anotherFunction();
+}
+anonymousGreet("janavi", function(){
+    console.log("This is an anonymous callback function.");
+});
+
+
+
+
+//callback function
+function greet3(name, callback){
+    console.log("Hello, " + name + "!");
+    callback();
+}  
+
+//callback  
+function sayGoodbye(){
+    console.log("Goodbye!");
+}   
+greet3("janavi", sayGoodbye);
+
